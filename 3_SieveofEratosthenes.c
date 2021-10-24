1b72a123c38bfb8ff2f28c1dc9709ae072a9048a
Nikhil Vinay <88549213+0024nv@users.noreply.github.com>
2021-10-24 13:42:27 +0530

    Update 3_SieveofEratosthenes.c

--- 3_SieveofEratosthenes.c
+++ 3_SieveofEratosthenes.c
@@ -26,23 +26,6 @@ int main()
     ull n ;
     printf("Enter n: ");
     scanf("%llu",&n);
-    // ull prime[n + 1];
-    // for(ull i=0;i<=n;i++)
-    // prime[i]=1;
-    // for (ull p = 2; p * p <= n; p++)
-    // {
-       
-    //     if (prime[p] == 1)
-    //     {
-
-    //         for (ull i = p * p; i <= n; i += p)
-    //             prime[i] = 0;
-    //     }
-    // }
-    // printf("print all prime no less than n: \n");
-    // for (ull p = 2; p <= n; p++)
-    //     if (prime[p])
-    //     printf("%llu ",p);
            
     SieveOfEratosthenes(n);
     return 0;
